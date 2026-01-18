03c566d423feb3dfaf29c3c9e60221dd53e79a3e
Hans Gumiran <hansjessieaugusto.gumiran@cvsu.edu.ph>
2026-01-18 23:02:29 +0800

    Update OrderStatus.java

--- OrderStatus.java
+++ OrderStatus.java
@@ -6,3 +6,8 @@ public enum OrderStatus {
     SHIPPED,
     CANCELLED
 }
+
+// GUMIRAN, HANS JESSIE AUGUSTO M.
+// BSCS-2D
+// AI USED: CHATGPT
+// LINK CONVO: https://chatgpt.com/share/696cf453-e8b0-8005-8304-112cef24edbd
