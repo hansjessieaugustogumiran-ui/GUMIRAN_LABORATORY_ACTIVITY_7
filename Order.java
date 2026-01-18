2e432e6be1a7cba66627b136cfc9d39c1288cbc0
Hans Gumiran <hansjessieaugusto.gumiran@cvsu.edu.ph>
2026-01-18 23:02:04 +0800

    Update Order.java

--- Order.java
+++ Order.java
@@ -54,3 +54,8 @@ public abstract class Order {
         this.status = OrderStatus.CANCELLED;
     }
 }
+
+// GUMIRAN, HANS JESSIE AUGUSTO M.
+// BSCS-2D
+// AI USED: CHATGPT
+// LINK CONVO: https://chatgpt.com/share/696cf453-e8b0-8005-8304-112cef24edbd
