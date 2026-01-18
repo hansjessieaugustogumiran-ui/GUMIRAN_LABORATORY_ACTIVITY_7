63c038bff21f26a2bcda0391f0b5a180e0f5c0d4
Hans Gumiran <hansjessieaugusto.gumiran@cvsu.edu.ph>
2026-01-18 22:55:47 +0800

    Update OnlineOrder.java

--- OnlineOrder.java
+++ OnlineOrder.java
@@ -19,3 +19,8 @@ public class OnlineOrder extends Order implements Payable {
         System.out.println("Order ID " + getOrderId() + " has been paid.");
     }
 }
+
+// GUMIRAN, HANS JESSIE AUGUSTO M.
+// BSCS-2D
+// AI USED: CHATGPT
+// LINK CONVO: https://chatgpt.com/share/696cf453-e8b0-8005-8304-112cef24edbd
