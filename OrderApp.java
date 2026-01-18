3a240d845e4f5193d04a679b4c38d90bd5dbc380
Hans Gumiran <hansjessieaugusto.gumiran@cvsu.edu.ph>
2026-01-18 23:02:17 +0800

    Update OrderApp.java

--- OrderApp.java
+++ OrderApp.java
@@ -38,3 +38,8 @@ public class OrderApp {
         }
     }
 }
+
+// GUMIRAN, HANS JESSIE AUGUSTO M.
+// BSCS-2D
+// AI USED: CHATGPT
+// LINK CONVO: https://chatgpt.com/share/696cf453-e8b0-8005-8304-112cef24edbd
