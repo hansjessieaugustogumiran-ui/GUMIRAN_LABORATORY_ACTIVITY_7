db1d01473ad7830997d16b75340baa5c7803a66d
Hans Gumiran <hansjessieaugusto.gumiran@cvsu.edu.ph>
2026-01-18 23:02:50 +0800

    Update Payable.java

--- Payable.java
+++ Payable.java
@@ -3,3 +3,8 @@ package com.store.payment;
 public interface Payable {
     void pay();
 }
+
+// GUMIRAN, HANS JESSIE AUGUSTO M.
+// BSCS-2D
+// AI USED: CHATGPT
+// LINK CONVO: https://chatgpt.com/share/696cf453-e8b0-8005-8304-112cef24edbd
